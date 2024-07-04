55f94fab6d1b8885203cdb30421106f300c4130f
glenndsilva1 <glenndsilva@rediffmail.com>
2024-07-04 14:25:30 +0530

    HundredDispender.java

---
+++ HundredDispender.java
@@ -0,0 +1,24 @@
+package com.servlet.cor.atm;
+
+public class HundredDispender extends CurrencyDispenserHandler {
+	public HundredDispender() {
+		super();
+	}
+	@Override
+	public void dispense(WithdrawalAmount curr) {
+		if(curr != null) {
+			double amount = curr.getAmount();
+			double remainder = amount;
+
+			if(amount >= 1000) {
+				int count = (int) (amount / 100);
+				remainder = amount % 100;
+				System.out.printf("Dispensing '%d' Rs.100 currency notes.\n", count);
+			}
+
+			if(remainder > 0 && this.nextDispensor != null){
+				this.nextDispensor.dispense(new WithdrawalAmount(remainder));
+			}
+		}
+	}
+}
